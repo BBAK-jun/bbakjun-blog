6b3b3945532118b2570bb1d8ae1a387c88bc5559
BBAKJUN <wnsguddl789@gmail.com>
2025-12-25 23:45:33 +0900

    fix: Append `/api` to the admin RPC client URL.

--- apps/blog/src/shared/lib/rpc.ts
+++ apps/blog/src/shared/lib/rpc.ts
@@ -2,4 +2,4 @@ import { BlogAdminApp } from '@apps/blog-admin/rpc';
 import { hc } from 'hono/client';
 import { env } from '@/env';
 
-export const client = hc<BlogAdminApp>(env.NEXT_PUBLIC_ADMIN_URL);
+export const client = hc<BlogAdminApp>(`${env.NEXT_PUBLIC_ADMIN_URL}/api`);
